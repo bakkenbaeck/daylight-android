762241947761d01de5895fd3e6f9fc205db1f427
Martin Tøften <martintoften@Martins-MacBook-Pro.local>
2016-12-14 10:52:53 +0100

    Added the half circle with different colors as pngs

--- app/src/main/java/com/bakkenbaeck/sol/ui/InfoFragment.java
+++ app/src/main/java/com/bakkenbaeck/sol/ui/InfoFragment.java
@@ -1,7 +1,6 @@
 package com.bakkenbaeck.sol.ui;
 
 import android.databinding.DataBindingUtil;
-import android.graphics.PorterDuff;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -22,6 +21,8 @@ public class InfoFragment extends Fragment {
     public static final String TAG = "InfoFragment";
     public static final String PHASE_NAME = "InfoFragment";
 
+    private FragmentInfoBinding binding;
+
     private boolean notifications;
     private String phaseName;
 
@@ -33,8 +34,6 @@ public class InfoFragment extends Fragment {
         f.setArguments(b);
         return f;
     }
-
-    private FragmentInfoBinding binding;
 
     @Nullable
     @Override
@@ -86,19 +85,49 @@ public class InfoFragment extends Fragment {
     }
 
     private void setColors(final String phaseName) {
-        CurrentPhase phase = new CurrentPhase(phaseName);
+        final CurrentPhase phase = new CurrentPhase(phaseName);
         final String infoMessage = this.getContext().getResources().getString(R.string.info_message);
         final String formatedInfo = infoMessage.replace("{color}", String.valueOf(ContextCompat
                 .getColor(this.getContext(), phase.getPrimaryColor())));
-        Spanned s = convertToHtml(formatedInfo);
+        final Spanned s = convertToHtml(formatedInfo);
         this.binding.infoMessage.setText(s);
 
-        this.binding.title.setTextColor(ContextCompat.getColor(this.getContext(), phase.getSecondaryColor()));
-        this.binding.sunCircle.setColorFilter(ContextCompat.getColor(this.getContext(), phase.getSecondaryColor()), PorterDuff.Mode.SRC);
-        this.binding.root.setBackgroundColor(ContextCompat.getColor(this.getContext(), phase.getBackgroundColor()));
-        this.binding.infoMessage.setTextColor(ContextCompat.getColor(this.getContext(), phase.getSecondaryColor()));
-        this.binding.notificationText.setTextColor(ContextCompat.getColor(this.getContext(), phase.getSecondaryColor()));
-        this.binding.notificationValue.setTextColor(ContextCompat.getColor(this.getContext(), phase.getPrimaryColor()));
+        final int color = phase.getBackgroundColor();
+        final int priColor = phase.getPrimaryColor();
+        final int secColor = phase.getSecondaryColor();
+
+        this.binding.title.setTextColor(ContextCompat.getColor(this.getContext(), secColor));
+        this.binding.root.setBackgroundColor(ContextCompat.getColor(this.getContext(), color));
+        this.binding.infoMessage.setTextColor(ContextCompat.getColor(this.getContext(), secColor));
+        this.binding.notificationText.setTextColor(ContextCompat.getColor(this.getContext(), secColor));
+        this.binding.notificationValue.setTextColor(ContextCompat.getColor(this.getContext(), priColor));
+
+        setSunDrawable(secColor);
+    }
+
+    public void setSunDrawable(final int color) {
+        switch (color) {
+            case R.color.sunrise_text: {
+                this.binding.sunCircle.setImageDrawable(ContextCompat.getDrawable(this.getContext(), R.drawable.half_circle_sunrise));
+                break;
+            }
+            case R.color.daylight_text: {
+                this.binding.sunCircle.setImageDrawable(ContextCompat.getDrawable(this.getContext(), R.drawable.half_circle_daylight));
+                break;
+            }
+            case R.color.sunset_text: {
+                this.binding.sunCircle.setImageDrawable(ContextCompat.getDrawable(this.getContext(), R.drawable.half_circle_daylight));
+                break;
+            }
+            case R.color.twilight_text: {
+                this.binding.sunCircle.setImageDrawable(ContextCompat.getDrawable(this.getContext(), R.drawable.half_circle_twilight));
+                break;
+            }
+            case R.color.night_text: {
+                this.binding.sunCircle.setImageDrawable(ContextCompat.getDrawable(this.getContext(), R.drawable.half_circle_night));
+                break;
+            }
+        }
     }
 
     public void update(final String phaseName) {
