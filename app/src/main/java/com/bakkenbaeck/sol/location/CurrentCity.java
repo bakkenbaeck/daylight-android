0c1a5f71cfb231c3bd1869f6a8e671018399aa45
csdodd <colinsdodd@gmail.com>
2017-10-28 17:39:06 +0200

    Handle being in an unknown location

--- app/src/main/java/com/bakkenbaeck/sol/location/CurrentCity.java
+++ app/src/main/java/com/bakkenbaeck/sol/location/CurrentCity.java
@@ -33,30 +33,51 @@ public class CurrentCity {
                 return address.getLocality();
             }
 
-            return defaultCity();
+            return defaultLocation();
         } catch (final IOException e) {
-            return defaultCity();
+            return defaultLocation();
         }
     }
 
-    public String getCityAndCountry(final double latitude, final double longitude) {
+    public String getCityAndCountry(final double latitude,
+                                    final double longitude) {
         final Geocoder geocoder = new Geocoder(context, Locale.getDefault());
         try {
             final List<Address> addresses = geocoder.getFromLocation(latitude, longitude, 1);
-            final Address address = addresses.get(0);
+            final Address address = getAddress(addresses);
 
             if (address == null) {
-                return defaultCity();
+                return defaultLocation();
             }
 
-            return String.format("%s, %s", address.getLocality(), address.getCountryName());
+            final String locality = getLocality(address);
+            final String country = getCountry(address);
+            return String.format("%s%s", locality, country);
         } catch (IOException e) {
-            return defaultCity();
+            return defaultLocation();
         }
     }
 
+    private Address getAddress(final List<Address> addresses) {
+        return addresses != null && addresses.size() > 0
+                ? addresses.get(0)
+                : null;
+    }
+
+    private String getLocality(final Address address) {
+        return address.getLocality() != null
+                ? address.getLocality()
+                : defaultLocation();
+    }
+
+    private String getCountry(final Address address) {
+        return address.getCountryName() != null
+                ? ", " + address.getCountryName()
+                : "";
+    }
+
     @NonNull
-    private String defaultCity() {
+    private String defaultLocation() {
         return context.getString(R.string.default_city);
     }
 }
